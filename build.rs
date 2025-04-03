74e78b61365a72dcc0e50c3674c1c9fb79162b1b
Zicklag <zicklag@katharostech.com>
2025-04-03 14:31:39 -0500

    build: just use pkg-config package directly instead of system-deps.

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    system_deps::Config::new().probe().unwrap();
+    #[cfg(target_os = "linux")]
+    {
+        pkg_config::Config::new().atleast_version("1").probe("x11").unwrap();
+        pkg_config::Config::new().atleast_version("1").probe("xtst").unwrap();
+    }
 }
